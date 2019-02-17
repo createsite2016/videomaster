c6933fe3e4f1ffbac4a2002618bbdf4d058c9db6
Andrey Galushko <createsite2016@gmail.com>
2019-02-17 20:21:58 +0300

    новые таблицы для БД

--- look_2019-02-17.sql
+++ look_2019-02-17.sql
@@ -7,7 +7,7 @@
 #
 # Адрес: localhost (MySQL 5.7.23)
 # Схема: look
-# Время создания: 2019-02-16 16:23:31 +0000
+# Время создания: 2019-02-17 11:02:51 +0000
 # ************************************************************
 
 
@@ -114,24 +114,53 @@ CREATE TABLE `work` (
   `link` varchar(255) DEFAULT NULL,
   `day` date DEFAULT NULL,
   `status` int(5) DEFAULT '1',
+  `sort` int(255) DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 LOCK TABLES `work` WRITE;
 /*!40000 ALTER TABLE `work` DISABLE KEYS */;
 
-INSERT INTO `work` (`id`, `name`, `link`, `day`, `status`)
+INSERT INTO `work` (`id`, `name`, `link`, `day`, `status`, `sort`)
 VALUES
-	(11,'Anlaym - Лунная пыль','https://www.youtube.com/watch?v=FosKso3eu2c','2019-02-16',1),
-	(12,'Алексей и Виктория','https://www.youtube.com/watch?v=uP0AsmgCWvU','2019-02-16',1),
-	(13,'Тысяча невест','https://www.youtube.com/watch?v=xyj4ERKLQ-c','2019-02-16',1),
-	(14,'Флобера & Focuss','https://www.youtube.com/watch?v=0hD4O92SFUU','2019-02-16',0),
-	(15,'CrosVord - Приглашение','https://www.youtube.com/watch?v=7Zdfex9CK2c','2019-02-16',1),
-	(17,'День Энергетика','https://www.youtube.com/watch?v=yu6807yXqyA','2019-02-16',1),
-	(18,'Твоя история','https://www.youtube.com/watch?v=B_srqvPhnok','2019-02-16',1),
-	(19,'Вячеслав и Ксения','https://www.youtube.com/watch?v=2o8G9MWKvho','2019-02-16',1),
-	(20,'Иван и Елизаветта ','https://www.youtube.com/watch?v=ndm-MSyjJ4Q','2019-02-16',1),
-	(21,'Профсоюз','https://www.youtube.com/watch?v=yqfc1gh8_KY','2019-02-16',0);
+	(11,'Anlaym - Лунная пыль','https://www.youtube.com/watch?v=FosKso3eu2c','2019-02-16',1,1),
+	(12,'Алексей и Виктория','https://www.youtube.com/watch?v=uP0AsmgCWvU','2019-02-16',1,2),
+	(13,'Тысяча невест','https://www.youtube.com/watch?v=xyj4ERKLQ-c','2019-02-16',1,3),
+	(14,'Флобера & Focuss','https://www.youtube.com/watch?v=0hD4O92SFUU','2019-02-16',0,4),
+	(15,'CrosVord - Приглашение','https://www.youtube.com/watch?v=7Zdfex9CK2c','2019-02-16',0,5),
+	(17,'День Энергетика','https://www.youtube.com/watch?v=yu6807yXqyA','2019-02-16',0,6),
+	(18,'Твоя история','https://www.youtube.com/watch?v=B_srqvPhnok','2019-02-16',1,7),
+	(19,'Вячеслав и Ксения','https://www.youtube.com/watch?v=2o8G9MWKvho','2019-02-16',1,8),
+	(20,'Иван и Елизаветта ','https://www.youtube.com/watch?v=ndm-MSyjJ4Q','2019-02-16',1,9),
+	(21,'Профсоюз','https://www.youtube.com/watch?v=yqfc1gh8_KY','2019-02-16',0,10),
+	(22,'Денис и Татьяна','https://www.youtube.com/watch?v=AZl5rH_H8AY','2019-02-16',1,11),
+	(23,'Финальная песня (КМК)','https://www.youtube.com/watch?v=kY7j71lAY_U','2019-02-16',1,12),
+	(24,'Sofi Arbuzik, AnNi feat SG (Magruff) - Тяпа-тяпа','https://www.youtube.com/watch?v=Pufx5o3IjYg','2019-02-16',1,13),
+	(25,'Сергей и Алина','https://www.youtube.com/watch?v=CUZDTGtW5jw','2019-02-16',1,14),
+	(26,'Николай и Юля','https://www.youtube.com/watch?v=P0293eadJ-E','2019-02-16',1,15),
+	(27,'Кропоткинский Медицинский Колледж 411 группа','https://www.youtube.com/watch?v=zFijkyi298g','2019-02-16',1,16),
+	(28,'Вкима - Рядом с Ней','https://www.youtube.com/watch?v=K-SiLgEXA_8','2019-02-16',1,17),
+	(29,'Вкима - В присутствии лИса','https://www.youtube.com/watch?v=ze15phiYbaw','2019-02-16',1,18),
+	(30,'Вкима - Она мой путь','https://www.youtube.com/watch?v=8FrKn74gzLU','2019-02-16',1,19),
+	(31,'Николай и Анна. Пешеходный переход.','https://www.youtube.com/watch?v=qeccbYMEFss','2019-02-16',0,20),
+	(32,'История Колледжа (Кропоткинский Медицинский Колледж)','https://www.youtube.com/watch?v=vAQXeZ2SHmw','2019-02-16',0,21),
+	(33,'S-FAME & КАRЯ - Скажи что такое любовь','https://www.youtube.com/watch?v=kQQXOx5nQ24','2019-02-16',0,22),
+	(34,'Борис и Наталья','https://www.youtube.com/watch?v=j886zcHloRU','2019-02-16',0,23),
+	(35,'Олег и Дарья','https://www.youtube.com/watch?v=OLVvCLKqZwc','2019-02-16',0,24),
+	(36,'День знаний (Кропоткинский Медицинский Колледж)','https://www.youtube.com/watch?v=aA43GVt5Y9g','2019-02-16',1,25),
+	(37,'Февральский - Найди Меня','https://www.youtube.com/watch?v=1KN41Ai765w','2019-02-16',1,26),
+	(38,'Социальный ролик \"Профилактика абортов\" КМК','https://www.youtube.com/watch?v=uLSGkqTzUj8','2019-02-16',0,27),
+	(39,'Февральский - Багровые зори [Алые тона]','https://www.youtube.com/watch?v=nsYYIETZAz4','2019-02-16',0,28),
+	(40,'Февральский - Гадкий утёнок [Алые тона]','https://www.youtube.com/watch?v=VDJpbUFRcyg','2019-02-16',0,29),
+	(41,'Февральский - Алые паруса [Алые тона]','https://www.youtube.com/watch?v=pCFAleyDESU','2019-02-16',0,30),
+	(42,'Зима сегодня [DARI film, Февральский Взгляд]','https://www.youtube.com/watch?v=cRof1NUCJPU','2019-02-16',0,31),
+	(43,'Кислород [DARI film, Февральский Взгляд]','https://www.youtube.com/watch?v=_Z46XvVbKt4','2019-02-16',0,32),
+	(44,'Мелодия души [DARI film, Февральский Взгляд]','https://www.youtube.com/watch?v=y9IqPEejGNI','2019-02-16',0,33),
+	(45,'Февральский - Домой в Париж [Алые тона]','https://www.youtube.com/watch?v=0gXtRJxpYL0','2019-02-16',0,34),
+	(46,'ALEX MERC ft ЛарисКа - Увязли во лжи [Февральский Взгляд]','https://www.youtube.com/watch?v=osg_9Tl55-Y','2019-02-16',0,35),
+	(47,'DabStep Counter Dance [Февральский Взгляд]','https://www.youtube.com/watch?v=0XQmwQLHCA4','2019-02-16',0,36),
+	(48,'Юрий Копиенко и Мария Базлюк - Расстояние [Февральский Взгляд]','https://www.youtube.com/watch?v=qrkSGkbmlzY','2019-02-16',0,37),
+	(49,'Dok - Мы далеко [Февральский Взгляд production]','https://www.youtube.com/watch?v=ybVMXZj68BM','2019-02-16',0,38);
 
 /*!40000 ALTER TABLE `work` ENABLE KEYS */;
 UNLOCK TABLES;
