e4316ac76fab90591d7046bad0bfd06fba5e2e56
Andrey Galushko <createsite2016@gmail.com>
2019-02-17 23:23:59 +0300

    объединил и сжал все JS и CSS

--- template/header.php
+++ template/header.php
@@ -4,10 +4,12 @@
     <title>Февральский взгляд</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
       <link rel="stylesheet" href="/css/andrey.css">
       <script type="text/javascript" src="/js/1.10.2.jquery.min.js"></script>
       <link rel="stylesheet" type="text/css" href="/css/lightcase.css">
+      <script type="text/javascript" src="/js/lightcase.js"></script>
   </head>
     <body>
