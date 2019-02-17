7cbe72e0d6113674ef7e2f48012262c1ace8bb5f
Andrey Galushko <createsite2016@gmail.com>
2019-02-17 23:07:38 +0300

    объединил и сжал все JS и CSS

--- template/header.php
+++ template/header.php
@@ -8,5 +8,28 @@
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,700" rel="stylesheet">
       <link rel="stylesheet" href="/css/andrey.css">
+
+<!--    <link rel="stylesheet" href="/css/open-iconic-bootstrap.min.css">-->
+<!--    <link rel="stylesheet" href="/css/animate.css">-->
+<!---->
+<!--    <link rel="stylesheet" href="/css/owl.carousel.min.css">-->
+<!--    <link rel="stylesheet" href="/css/owl.theme.default.min.css">-->
+<!--    <link rel="stylesheet" href="/css/magnific-popup.css">-->
+<!---->
+<!--    <link rel="stylesheet" href="/css/aos.css">-->
+<!---->
+<!--    <link rel="stylesheet" href="/css/ionicons.min.css">-->
+<!---->
+<!--    <link rel="stylesheet" href="/css/bootstrap-datepicker.css">-->
+<!--    <link rel="stylesheet" href="/css/jquery.timepicker.css">-->
+<!---->
+<!---->
+<!--    <link rel="stylesheet" href="/css/flaticon.css">-->
+<!--    <link rel="stylesheet" href="/css/icomoon.css">-->
+<!--    <link rel="stylesheet" href="/css/style.css">-->
+<!---->
+      <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+      <link rel="stylesheet" type="text/css" href="/css/lightcase.css">
+      <script type="text/javascript" src="/js/lightcase.js"></script>
   </head>
     <body>
