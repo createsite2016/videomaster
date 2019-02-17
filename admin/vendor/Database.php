3e5fadc90fd499610b0277b97fb6ade8c4db5ce6
Andrey <createsite2016@gmail.com>
2019-02-17 13:03:09 +0300

    добавил класс БД в игнор

--- admin/vendor/Database.php
+++ admin/vendor/Database.php
@@ -8,7 +8,7 @@ class Database{
 
 
     // Соединение с БД
-        public function __construct($username = "root", $password = "root", $host="localhost", $dbname = "look", $options = []){
+        public function __construct($username = "cw45544_look", $password = "16213150", $host="localhost", $dbname = "cw45544_look", $options = []){
             $this->isConn = TRUE;
         try {
             $this->datab = new PDO("mysql:host={$host};dbname={$dbname};charset=utf8", $username, $password, $options);
