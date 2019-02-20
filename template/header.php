b48a805a9ab84364472a2f44b028e540edcc3375
Andrey Galushko <createsite2016@gmail.com>
2019-02-21 00:47:16 +0300

    добавил SEO ключи

--- template/header.php
+++ template/header.php
@@ -1,7 +1,9 @@
 <!DOCTYPE html>
 <html lang="ru">
   <head>
-    <title>Февральский взгляд</title>
+    <title>Съемка свадеб Гулькевичи Кропоткин Краснодар | Февральский взгляд</title>
+    <meta name="description" content="Съемка свадьбы, видео, гулькевичи, кропоткин, цены, отзывы">
+    <meta name="keywords" content="свадьба, гулькевичи, съемка, кропоткин, видео">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
